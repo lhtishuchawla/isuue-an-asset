2939fb1d85e85cd1bd9db398ffbb08dac1938a6f
Ishu_IshuChawla <ishu@leewayhertz.com>
2022-06-16 18:16:10 +0530

    updated

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
-const stellar = require("stellar-sdk");
 const express = require("express");
-const crypto = require("crypto");
+const app = express();
 const path = require("path");
 const cors = require("cors");
-const app = express();
 app.use(cors());
 
-/**
- * Status Check End Point
- */
-app.get("/", (req, res) => {
-  res.json({ status: "Stellar Toml APIs are Deployed!!!!!" });
-});
-
-/**
- * -----------SEP1 Implimentation Start---------------//
- */
-app.get("/.well-known/stellar.toml", (req, res, next) => {
-  const options = { root: path.join(__dirname, "public") };
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("content-type", "text/plain");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "Deployed!!!!!",
+  });
+});
+
+app.get("/.well-known/stellar.toml", (req, res, next) => {
+  const options = {
+    root: path.join(__dirname, "public"),
+  };
+
   res.sendFile("stellar.toml", options);
 });
-//-----------SEP1 Implimentation End------------------//
 
-/**
- * Starting Server
- */
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is Running Locally on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
